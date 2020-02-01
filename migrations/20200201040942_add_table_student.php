ebcb21422afc93a2d41700b790059ed6ad71cc2f
Rozum Roman <rrozum@team.amocrm.com>
2020-02-01 04:38:49 +0300

    Добавил миграцию student

---
+++ migrations/20200201040942_add_table_student.php
@@ -0,0 +1,47 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class AddTableStudent extends Migration
+{
+    const TABLE = 'student';
+
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $container = $this->getContainer();
+
+        /** @var \Illuminate\Database\Capsule\Manager $db */
+        $db = $container['db'];
+        $schema = $db->schema();
+        $connection = $schema->getConnection();
+
+        $schema->create(
+            self::TABLE,
+            function (\Illuminate\Database\Schema\Blueprint $table) use ($connection) {
+                $table->unsignedInteger('id')->primary();
+                $table->char('name', 250);
+                $table->timestamp('birth');
+                $table->char('phone', 15)->nullable();
+                $table->char('email', 50)->nullable();
+                $table->char('type', 50)->nullable();
+                $table->unsignedInteger('course_id');
+                $table->unsignedInteger('school_id');
+            }
+        );
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        $container = $this->getContainer();
+        /** @var \Illuminate\Database\Capsule\Manager $db */
+        $db = $container['db'];
+        $schema = $db->schema();
+        $schema->dropIfExists(self::TABLE);
+    }
+}

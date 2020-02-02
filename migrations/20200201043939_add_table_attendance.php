4e74c3571fc1c48d6b668f6b2684059b3b533132
Rozum Roman <rrozum@team.amocrm.com>
2020-02-02 16:06:24 +0300

    Добавил новую таблицу attendance

---
+++ migrations/20200201043939_add_table_attendance.php
@@ -0,0 +1,42 @@
+<?php
+
+use Phpmig\Migration\Migration;
+
+class AddTableAttendance extends Migration
+{
+    const TABLE = 'attendance';
+
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        $container = $this->getContainer();
+
+        /** @var \Illuminate\Database\Capsule\Manager $db */
+        $db = $container['db'];
+        $schema = $db->schema();
+        $connection = $schema->getConnection();
+
+        $schema->create(
+            self::TABLE,
+            function (\Illuminate\Database\Schema\Blueprint $table) use ($connection) {
+                $table->unsignedInteger('student_id')->primary();
+                $table->timestamp('date');
+                $table->tinyInteger('attend');
+            }
+        );
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        $container = $this->getContainer();
+        /** @var \Illuminate\Database\Capsule\Manager $db */
+        $db = $container['db'];
+        $schema = $db->schema();
+        $schema->dropIfExists(self::TABLE);
+    }
+}

ac0ab7c63ecc60ff1148aa8fe022295c938b8635
Rozum Roman <rrozum@team.amocrm.com>
2020-02-02 16:40:45 +0300

    Новая модель Course

---
+++ src/App/src/Entity/Course.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'hours',
+        'location',
+    ];
+
+    protected $casts = [
+        'hours' => 'integer',
+        'id' => 'integer',
+    ];
+}

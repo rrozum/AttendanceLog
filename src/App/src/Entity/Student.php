05d8d7a9e6faf4949c62934d43005422fc282bbe
Rozum Roman <rrozum@team.amocrm.com>
2020-02-02 16:41:56 +0300

    Готовая модель Student

--- src/App/src/Entity/Student.php
+++ src/App/src/Entity/Student.php
@@ -1,10 +1,30 @@
 <?php
-
+declare(strict_types=1);
 
 namespace App\Entity;
 
 
-class Student
-{
+use Illuminate\Database\Eloquent\Model;
 
+class Student extends Model
+{
+    protected $primaryKey = 'id';
+
+    protected $dates = [
+        'birth',
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+    ];
+
+    protected $fillable = [
+        'name',
+        'birth',
+        'phone',
+        'email',
+        'type',
+        'course_id',
+        'school_id',
+    ];
 }
